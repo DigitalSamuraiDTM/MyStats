8ae2d618bb9d00cfffb30a004b38692e2eca309b
Andrew <nugaev.andrei@gmail.com>
2021-04-22 01:01:58 +0300

    New stats create columns

---
+++ app/src/main/java/com/mystats/mystats/new_statistic/AdapterRowsStats.kt
@@ -0,0 +1,26 @@
+package com.mystats.mystats.new_statistic
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterRowsStats() : RecyclerView.Adapter<AdapterRowsStats.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //var
+        init {
+          //todo init object
+        }
+
+    }
+    //todo типы данных. Сделать класс, который в себе будет хранить
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+}
