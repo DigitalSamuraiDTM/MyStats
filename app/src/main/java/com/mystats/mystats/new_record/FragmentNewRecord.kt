b64cb0e3b6b1433b1ef53f7adea2ec393ed2558a
Andrew <nugaev.andrei@gmail.com>
2021-07-08 03:06:09 +0300

    загрузочный экран при добавлении записи

--- app/src/main/java/com/mystats/mystats/new_record/FragmentNewRecord.kt
+++ app/src/main/java/com/mystats/mystats/new_record/FragmentNewRecord.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.mystats.mystats.AdapterRecord
 import com.mystats.mystats.R
@@ -20,6 +22,8 @@ class FragmentNewRecord : Fragment() {
     private lateinit var buttonFinish : Button
     private lateinit var  presenter : PresenterNewRecord
     private lateinit var  adapterRecord : AdapterRecord
+    private lateinit var layoutData : ConstraintLayout
+    private lateinit var layoutLoading : ConstraintLayout
     override fun onAttach(context: Context) {
         presenter = PresenterNewRecord(this)
         super.onAttach(context)
@@ -32,6 +36,8 @@ class FragmentNewRecord : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        layoutData = view.findViewById(R.id.fr_newRecord_layout_allData);
+        layoutLoading = view.findViewById(R.id.fr_newRecord_layout_loading);
         recyclerRecord = view.findViewById(R.id.fr_newRecord_recycler_record)
         buttonFinish = view.findViewById(R.id.fr_newRecord_button_finish)
         buttonFinish.setOnClickListener{
@@ -56,14 +62,18 @@ class FragmentNewRecord : Fragment() {
     }
 
     public fun showLoading(){
-        //TODO доделать, плюс убрать лесенку (попытаться)
+        layoutData.visibility = View.GONE
+        layoutLoading.visibility = View.VISIBLE
     }
 
     public fun showDataLayout(){
-
+        layoutData.visibility = View.VISIBLE
+        layoutLoading.visibility = View.GONE
     }
 
     public fun showError(){
-
+        layoutData.visibility = View.VISIBLE
+        layoutLoading.visibility = View.GONE
+        Toast.makeText(requireActivity(),"Something block our magic\n Check your connection and try again", Toast.LENGTH_SHORT).show();
     }
 }
